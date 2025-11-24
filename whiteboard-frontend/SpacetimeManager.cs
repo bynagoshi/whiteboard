95522b251d1b3e19a121b2d8fed5594a7469f01c
bynagoshi <bynagoshi@gmail.com>
2025-11-24 00:02:18 -0500

    Added Godot part, subscribed to tables

--- whiteboard-frontend/SpacetimeManager.cs
+++ whiteboard-frontend/SpacetimeManager.cs
@@ -1,15 +1,47 @@
 using Godot;
 using System;
+using SpacetimeDB.Types;
+using SpacetimeDB.ClientApi;
 
-public partial class SpacetimeManager : Control
+public partial class SpacetimeManager : Node
 {
+	public DbConnection Client;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		ConnectToDb();
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Client?.FrameTick();
+	}
+	
+	private void ConnectToDb()
+	{
+		const string HOST = "wss://maincloud.spacetimedb.com";
+		const string DB_NAME = "whiteboard";
+		
+		Client = DbConnection
+			.Builder()
+			.WithUri(HOST)
+			.WithModuleName(DB_NAME)
+			.OnConnect((conn, identity, token) =>
+			{
+				
+				conn.SubscriptionBuilder()
+					.OnApplied(ctx => { GD.Print("Subscribed");})
+					//.Subscribe(new string[] 
+					//{	
+						//"SELECT * FROM board",
+						//"SELECT * FROM stroke"
+					//})
+					.SubscribeToAllTables();
+			})
+			.Build();
+			
 	}
 }
